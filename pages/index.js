ef3664321e05fa9836544d21a52b50e276014a31
Mareike <mareike.brandt@hotmail.de>
2022-10-11 14:55:37 +0900

    feat: updated portfolio projects

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ import web3 from "../public/web3.png";
 import web4 from "../public/web4.jpg";
 import web5 from "../public/web5.png";
 import web6 from "../public/web6.jpeg";
+import web7 from "../public/web7.png";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
@@ -166,6 +167,19 @@ export default function Home() {
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
             <div className="basis-1/3 flex-1">
               <Image
+                src={web7}
+                className="rounded-lg object-cover"
+                alt=""
+                width={"90%"}
+                height={"90%"}
+                layout="responsive"
+              />
+              <a className="dark:text-amber-400 text-lg font-medium py-2 ">
+                studycafe (in progress)
+              </a>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
                 src={web1}
                 className="rounded-lg object-cover"
                 alt=""
@@ -255,22 +269,9 @@ export default function Home() {
               </a>
             </div>
             <div className="basis-1/3 flex-1">
-              <Image
-                src={web6}
-                className="rounded-lg object-cover"
-                alt=""
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-              />
-              <a
-                href="https://minimalist-lyart.vercel.app/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="dark:text-amber-400 text-lg font-medium py-2 "
-              >
+              <a className="text-amber-600 text-xl font-medium py-2">
                 {" "}
-                more projects coming soon!
+                ...more projects coming soon!
               </a>
             </div>
           </div>
